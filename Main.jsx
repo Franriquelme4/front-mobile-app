a7c1a818e5038f8a8cbffd3ff9bb9cb0a19e9aa6
mgueyraud <mgueyraud.junior@gmail.com>
2023-11-12 20:26:38 -0300

    Safe area for dynamic islands

--- Main.jsx
+++ Main.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { SafeAreaView } from "react-native";
 import { Route, Navigate, Routes } from 'react-router-native';
 import Categorias from "./src/screens/Categorias";
 import Home from "./src/screens/Home";
+import { View } from "native-base";
 
 const Main = () => {
     return (
-        <View style={{ flex: 1, padding: 30 }} >
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/categorias' element={<Categorias />} />
-                <Route path='*' element={<Navigate to='/'/>} />
-            </Routes>
-        </View>
+        <SafeAreaView>
+            <View px={5}>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/categorias' element={<Categorias />} />
+                    <Route path='*' element={<Navigate to='/'/>} />
+                </Routes>
+            </View>
+        </SafeAreaView>
     );
 }
 
