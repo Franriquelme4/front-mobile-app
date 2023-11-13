ffc05cf62e4c6b17871b719273762556da788da9
Francisco Riquelme <francisco.riquelme@konecta.com.py>
2023-11-13 10:21:58 -0300

    Horarios

---
+++ src/constants/horariosAtencion.js
@@ -0,0 +1,14 @@
+export const turnos= [
+    { id:1, inicio: '09:00', fin: '10:00' },
+    { id:2 ,inicio: '10:00', fin: '11:00' },
+    { id:3 ,inicio: '11:00', fin: '12:00' },
+    { id:4 ,inicio: '12:00', fin: '13:00' },
+    { id:5 ,inicio: '13:00', fin: '14:00' },
+    { id:6 ,inicio: '14:00', fin: '15:00' },
+    { id:7 ,inicio: '15:00', fin: '16:00' },
+    { id:8 ,inicio: '16:00', fin: '17:00' },
+    { id:9 ,inicio: '17:00', fin: '18:00' },
+    { id:10 ,inicio: '18:00', fin: '19:00' },
+    { id:11 ,inicio: '19:00', fin: '20:00' },
+    { id:12 ,inicio: '20:00', fin: '21:00' },
+  ];
